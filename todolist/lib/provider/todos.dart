bfc3c169654062ee7637a2179514fb08ad0877a4
Kyle <kyle_sanchez99@yahoo.com>
2023-04-08 23:15:57 +0800

    Implementing the list view builder for the todo list

--- todolist/lib/provider/todos.dart
+++ todolist/lib/provider/todos.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
-class TodosProvider extends ChangeNotifier {}
+import '../model/todo.dart';
+
+class TodosProvider extends ChangeNotifier {
+  final List<Todo> _todos = [
+    Todo(createdTime: DateTime.now(), title: 'Walk the dog'),
+    Todo(createdTime: DateTime.now(), title: 'Billies birthday')
+  ];
+
+  List<Todo> get todos => _todos.where((todo) => todo.isDone == false).toList();
+}
--- todolist/lib/widget/todo_list_widget.dart
+++ todolist/lib/widget/todo_list_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../model/todo.dart';
+import 'package:todolist/provider/todos.dart';
 import 'todo_widget.dart';
 
 class TodoListWidget extends StatelessWidget {
@@ -9,7 +8,16 @@ class TodoListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TodoWidget(
-        todo: Todo(createdTime: DateTime.now(), title: 'Walk the dog'));
+    final provider = Provider.of<TodosProvider>(context);
+    final todos = provider.todos;
+
+    return ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, index) {
+          final todo = todos[index];
+          return TodoWidget(todo: todo);
+        },
+        itemCount: todos.length);
   }
 }
